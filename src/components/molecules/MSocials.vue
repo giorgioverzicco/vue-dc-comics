e38c8b86f02dbcfda1548ac8ff5cec090a511432
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-06-01 20:00:36 +0200

    add footer bottom

---
+++ src/components/molecules/MSocials.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="m-socials">
+    <AText text="Follow us" />
+    <AIcon v-for="(icon, idx) in icons" :key="idx" :icon="icon" />
+  </div>
+</template>
+
+<script>
+import AIcon from "../atoms/AIcon.vue";
+import AText from "../atoms/AText.vue";
+export default {
+  components: { AText, AIcon },
+  data() {
+    return {
+      icons: [
+        {
+          img: "footer-facebook.png",
+          alt: "Facebook",
+        },
+        {
+          img: "footer-twitter.png",
+          alt: "Twitter",
+        },
+        {
+          img: "footer-youtube.png",
+          alt: "YouTube",
+        },
+        {
+          img: "footer-pinterest.png",
+          alt: "Pinteres",
+        },
+        {
+          img: "footer-periscope.png",
+          alt: "Periscope",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/style/mixins";
+
+.m-socials {
+  @include flex();
+  gap: var(--spacing-4);
+
+  .a-text {
+    font-size: var(--fs-xl);
+    font-weight: var(--fw-bold);
+    color: var(--clr-primary-500);
+    text-transform: uppercase;
+    margin-right: var(--spacing-2);
+  }
+}
+</style>
--- src/components/organisms/OFooterBottom.vue
+++ src/components/organisms/OFooterBottom.vue
@@ -1,11 +1,31 @@
 <template>
-  <div></div>
+  <div class="o-footerBottom">
+    <div class="container">
+      <AButton class="-primary -uppercase" text="Sign-up now!" />
+      <MSocials />
+    </div>
+  </div>
 </template>
 
 <script>
+import AButton from "../atoms/AButton.vue";
+import MSocials from "../molecules/MSocials.vue";
 export default {
+  components: { AButton, MSocials },
   name: "OFooterBottom",
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import "@/assets/style/mixins";
+
+.o-footerBottom {
+  color: var(--clr-secondary-050);
+  background-color: var(--clr-secondary-800);
+
+  .container {
+    @include flex(space-between);
+    padding-block: var(--spacing-6);
+  }
+}
+</style>
