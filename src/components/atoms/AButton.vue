9ccb6e269355a29b03e371dfcf5aa20b12df8b47
Giorgio Verzicco <giorgioverzicco00@gmail.com>
2022-06-06 17:21:18 +0200

    add button atom

---
+++ src/components/atoms/AButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <button class="a-button">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "AButton",
+  props: {
+    text: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.a-button {
+  padding: var(--spacing-3) var(--spacing-5);
+  font-size: var(--fs-sm);
+  color: inherit;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+
+  &.-primary {
+    font-weight: var(--fw-bold);
+    color: var(--clr-secondary-050);
+    background-color: var(--clr-primary-600);
+  }
+
+  &.-large {
+    padding-inline: var(--spacing-8);
+  }
+
+  &.-uppercase {
+    text-transform: uppercase;
+  }
+}
+</style>
